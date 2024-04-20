ac44800645004ae3629fc8e4a49917754a36f285
Misbah Shakil <141862743+misbahshakil233@users.noreply.github.com>
2024-04-20 17:39:52 +0500

    done project
    student Management project done

---
+++ index.js
@@ -0,0 +1,51 @@
+// how to add students in typescript?
+//solution:
+//class
+class Student {
+    name;
+    age;
+    id;
+    courses;
+    balance;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.id = this.generateUniqueId(); // Correctly call the generateUniqueId method
+        this.courses = [];
+        this.balance = 0;
+    }
+    // Method to generate a unique ID
+    generateUniqueId() {
+        return Math.floor(10000 + Math.random() * 90000).toString();
+    }
+    // Enroll new courses
+    enrollnewCourses(courses) {
+        this.courses.push(...courses);
+        console.log(`${this.name} has been enrolled in the courses: ${courses.join(', ')}`);
+    }
+    // View balance
+    viewBalance() {
+        console.log(`${this.name} has a balance of $${this.balance}`);
+    }
+    // Pay tuition fees
+    // payTuition(amount: number) {
+    //     if (amount <= this.balance) {
+    //         this.balance -= amount;
+    //         console.log(`${this.name} has paid $${amount} towards tuition fees.`);
+    //         this.viewBalance();
+    //     } else {
+    //         console.log(`${this.name} does not have sufficient balance to pay $${amount}.`);
+    //     }
+    // }
+    // Display student information
+    displayFunction() {
+        console.log(`Name: ${this.name}, Age: ${this.age}, Id: ${this.id}`);
+        console.log(`Enrolled Courses: ${this.courses.join(', ')}`);
+    }
+}
+let s1 = new Student("ali", 12);
+s1.enrollnewCourses(["sst", "urdu"]);
+// s1.payTuition(500);
+s1.viewBalance();
+s1.displayFunction();
+export {};
